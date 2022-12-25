909b281d4c2396a343f6ccb561cdee3d29062459
Tripti Sharma <sharmatripti526@gmail.com>
2022-12-25 11:43:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(function (req, res, next) {
   );
   next();
 });
-const port = 2410;
+const port = process.env.PORT || 2410;
 app.listen(port, () => console.log(`Node app listening on port ${port}!`));
 
 let { studentsData } = require("./data.js");
